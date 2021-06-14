3e39ebcebb7c1e8c1048aec34ec240593bc489dc
William Chislett <william.chislett@alsoenergy.com>
2021-06-14 08:09:00 -0600

    initial commit

---
+++ Athens/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Athens.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Athens.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
